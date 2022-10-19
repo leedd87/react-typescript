2877dac594c9abf3e82005b87f6f33575aaef663
Lee David <leedav87@gmail.com>
2022-10-19 16:17:55 -0300

    tiposbasicos typscript

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TiposBasicos } from './typescript/TiposBasicos'
 const App = () => {
   return (
     <div className='mt-2'><h1>Introduccion a Typescript React</h1>
+    <hr/>
     <TiposBasicos/>
     </div>
   )
--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 export const TiposBasicos = () => {
-  return (
-    <>
-        <h3>Tipos basicos</h3>
+	let nombre: string = 'Fernando'
+	const edad: number = 35
+	const estaActivo: boolean = true
 
-    </>
-  )
+	//array con strings
+	const poderes: string[] = [] //['Velocidad', 'Volar', 'Respirar en el agua']
+	return (
+		<>
+			<h3>Tipos basicos</h3>
+			{nombre},{edad}, {estaActivo ? 'activo' : 'no activo'}
+		</>
+	)
 }
