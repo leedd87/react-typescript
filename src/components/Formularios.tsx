9e4d5ff5f84860eab125c5cd1d93975c55935a17
Lee David <leedav87@gmail.com>
2022-11-11 11:07:10 -0300

    done react typescript introduction

--- src/components/Formularios.tsx
+++ src/components/Formularios.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm } from '../hooks/useForm'
 
 export const Formularios = () => {
+	const { formulario, onChange, email, password } = useForm({
+		email: 'test@test.com',
+		password: '123456',
+	})
+
 	return (
 		<>
 			<h3>Formularios</h3>
@@ -8,12 +14,23 @@ export const Formularios = () => {
 				type="text"
 				className="form-control"
 				placeholder="Email"
+				value={email}
+				onChange={({ target }) => {
+					onChange(target.value, 'email')
+				}}
 			/>
 			<input
 				type="text"
 				className="form-control mt-2 mb-2"
 				placeholder="Password"
+				value={password}
+				onChange={({ target }) => {
+					onChange(target.value, 'password')
+				}}
 			/>
+			<code>
+				<pre>{JSON.stringify(formulario, null, 4)}</pre>
+			</code>
 		</>
 	)
 }
